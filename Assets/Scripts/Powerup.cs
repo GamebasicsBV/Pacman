90679254cb5712b1cb9407ad137d4d414cd1b78a
Henk <lwhuybrechts@gmail.com>
2019-02-19 11:17:08 +0100

    nieuwe powerup platen

---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Powerup : MonoBehaviour {
+
+    private GameManager gm;
+    public Sprite sprites;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    gm = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        if( gm == null )    Debug.Log("Powerup.cs did not find Game Manager!");
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.name == "pacman" && !GameManager.isAnimatorFlipped)
+        {
+            //    if (new System.Random().Next(0, 2) < 1)
+            //        gm.ScareGhosts();
+            //    else
+            //        gm.FlipAnimator();
+
+            Destroy(gameObject);
+        }
+    }
+}
