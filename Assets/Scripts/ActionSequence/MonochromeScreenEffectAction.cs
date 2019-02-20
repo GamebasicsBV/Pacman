edd4bd6027a53239f7d236413dad275966c3303c
SofieGB <40756356+SofieGB@users.noreply.github.com>
2019-02-20 17:04:39 +0100

    added a monochrome effect

---
+++ Assets/Scripts/ActionSequence/MonochromeScreenEffectAction.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class MonochromeScreenEffectAction : IActionSequenceAction {
+
+	public float Duration = 0.07f;
+
+	override public void Invoke() {
+		MonochromeEffect.StartDoingTheMonochrome(Duration);
+	}
+
+	override public void InvokeAndDoOnDone(Action onDone) {
+		Invoke();
+		onDone.Invoke();
+	}
+}
---
+++ Assets/Scripts/MonochromeEffect.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class MonochromeEffect : MonoBehaviour {
+	public Material Material;
+	public static MonochromeEffect instance;
+	private float monochromeCountdownTimer = 0f;
+
+	public static void StartDoingTheMonochrome(float time) {
+		if (instance && instance.Material) {
+			instance.monochromeCountdownTimer = time;
+		}
+	}
+
+	// Start is called before the first frame update
+	void Start() {
+		instance = this;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (monochromeCountdownTimer > 0) {
+			monochromeCountdownTimer -= Time.deltaTime;
+		}
+
+		if (Input.GetKeyDown(KeyCode.P)) {
+			ColorDistortEffect.StartDoingTheColorDistort(0.07f);
+		}
+	}
+
+	void OnRenderImage(RenderTexture source, RenderTexture destination) {
+		if (monochromeCountdownTimer > 0) {
+			Graphics.Blit(source, destination, Material);
+		}
+		else {
+			// horrible, just disable the component. but hey i'm lazy
+			Graphics.Blit(source, destination);
+		}
+	}
+}
