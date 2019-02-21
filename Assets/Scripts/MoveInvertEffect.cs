30fd4ca3dea56a6b9e36ee86b76b1062cbddddfa
SofieGB <40756356+SofieGB@users.noreply.github.com>
2019-02-21 14:23:42 +0100

    effect made for when you are movement inverted

---
+++ Assets/Scripts/MoveInvertEffect.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class MoveInvertEffect : MonoBehaviour {
+	public Material Material;
+	public float Heaviness = 0.2f;
+	private bool effectGoing = false;
+	private static MoveInvertEffect instance;
+	public Vector2 scrollDirection = new Vector2(0.01f, 0.02f);
+	float timePassedX;
+	float timePassedY;
+
+	public static void StartDoingTheEffect() {
+		if (instance && instance.Material) {
+			instance.effectGoing = true;
+		}
+	}
+
+	public static void StopDoingTheEffect() {
+		if (instance && instance.Material) {
+			instance.effectGoing = false;
+		}
+	}
+
+	private void Start() {
+		instance = this;
+	}
+
+	void Update() {
+		timePassedX += Time.deltaTime;
+		timePassedY += Time.deltaTime;
+		float cycleTimeX = 1 / scrollDirection.x;
+		if (timePassedX >= cycleTimeX) {
+			timePassedX -= cycleTimeX;
+		}
+		float cycleTimeY = 1 / scrollDirection.y;
+		if (timePassedY >= cycleTimeY) {
+			timePassedY -= cycleTimeY;
+		}
+		Material.SetColor("_EffectOffset", new Color(scrollDirection.x * timePassedX, scrollDirection.y * timePassedY, 0, 0));
+		if (effectGoing) {
+			Material.SetFloat("_ColorMultiplier", Heaviness);
+		}
+		else {
+			Material.SetFloat("_ColorMultiplier", 0f);
+		}
+
+		if (Input.GetKeyDown(KeyCode.I)) {
+			StartDoingTheEffect();
+		}
+		if (Input.GetKeyUp(KeyCode.I)) {
+			StopDoingTheEffect();
+		}
+	}
+
+	void OnRenderImage(RenderTexture source, RenderTexture destination) {
+		if (effectGoing) {
+			Graphics.Blit(source, destination, Material);
+		}
+		else {
+			// horrible, just disable the component. but hey i'm lazy
+			Graphics.Blit(source, destination);
+		}
+	}
+}
---
+++ Assets/Shaders/InverseMovementShader.shader
@@ -0,0 +1,51 @@
+﻿Shader "Hidden/InverseMovementShader" {
+	Properties{
+		_MainTex("Screen Texture", 2D) = "white" {}
+		_MyNormalMap("Normal map", 2D) = "bump" {}
+		_ColorMultiplier("Color Multiplier", Float) = 0.1
+		_EffectOffset("Effect Offset", Color) = (0, 0, 0, 0)
+		_Color("Color Multiplier", Color) = (1, 0, 0, 0)
+	}
+	SubShader {
+		Pass {
+
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma fragmentoption ARB_precision_hint_fastest
+			#include "UnityCG.cginc"
+
+			sampler2D _MainTex, _MyNormalMap;
+			float4 _MainTex_TexelSize;
+			float _OffsetMultiplier, _ColorMultiplier;
+			float4 _Color, _EffectOffset;
+
+			struct v2f {
+				float4 vertex : SV_POSITION;
+				float2 uv     : TEXCOORD0;
+			};
+
+			v2f vert(appdata_img v) {
+				v2f o;
+				#if UNITY_UV_STARTS_AT_TOP
+				if (_MainTex_TexelSize.y < 0)
+						v.texcoord.y = 1 - v.texcoord.y;
+				#endif
+
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.texcoord;
+
+				return o;
+			}
+
+			fixed4 frag(v2f i) : COLOR
+			{
+				fixed4 colorBrightness = _ColorMultiplier * tex2D(_MyNormalMap, i.uv + float2(_EffectOffset.x, _EffectOffset.y)) * _Color;
+				fixed4 tex_screen = tex2D(_MainTex, i.uv);
+				return tex_screen + colorBrightness;
+			}
+
+			ENDCG
+		}
+	}
+}
