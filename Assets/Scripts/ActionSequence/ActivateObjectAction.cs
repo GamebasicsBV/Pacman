b42bee873027463cfe8927a2925c55ca8e515d81
SofieGB <40756356+SofieGB@users.noreply.github.com>
2019-02-21 11:16:07 +0100

    new option to make environment react to pacman

---
+++ Assets/Scripts/ActionSequence/ActivateObjectAction.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+
+public class ActivateObjectAction : IActionSequenceAction {
+
+	public bool SetActive = true;
+	public GameObject GameObject;
+
+	override public void Invoke() {
+		if (GameObject) {
+			GameObject.SetActive(SetActive);
+		}
+	}
+
+	override public void InvokeAndDoOnDone(Action onDone) {
+		Invoke();
+		onDone.Invoke();
+	}
+}
